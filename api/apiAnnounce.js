e6010e4c9dde51234ad42ea45e2447c7e03dfa05
unknown <2889250797@qq.com>
2020-03-29 14:08:23 +0800

    add deliver_announce

--- api/apiAnnounce.js
+++ api/apiAnnounce.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-03-28 17:41:53
- * @LastEditTime: 2020-03-28 19:52:18
+ * @LastEditTime: 2020-03-29 13:56:14
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \毕业设计\server\api\apiAnnounce.js
@@ -9,9 +9,10 @@
 let mysql = require('./index.js'); 
 function getAnnounceInfo(sqlWord,callback){
     let connection = mysql(); 
-    // let query = "select * from public_announce where time between ? and ?"; 
-    let query = "select * from public_announce"; 
-    connection.query(query,sqlWord,(err,data)=>{
+    let query = "select * from public_announce where time between ? and ?"; 
+    let params = [sqlWord.startChooseDate,sqlWord.endChooseDate];
+    console.log(params)
+    connection.query(query,params,(err,data)=>{
         if(err){
             callback(err)
         }else{ 
@@ -20,9 +21,55 @@ function getAnnounceInfo(sqlWord,callback){
     })
     connection.end()
 }
+
+function deliverAnnounce(sqlWord,callback){
+    let connection = mysql(); 
+    // let query = "select * from public_announce where time between ? and ?"; 
+    let query = "insert into public_announce(announce_title,announce_content,time,userid) values(?,?,?,?)";
+    let params = [sqlWord.title,sqlWord.content,sqlWord.time,sqlWord.userid] 
+    connection.query(query,params,(err,data)=>{
+        if(err){
+            callback(err);
+            console.log(err)
+        }else{ 
+            callback(data)
+        }
+    })
+    connection.end()
+}
  
+function getMyDelivetAnnounce(sqlWord,callback){
+    let connection = mysql(); 
+    let query = "select * from public_announce where userid = ?"; 
+    connection.query(query,sqlWord,(err,data)=>{
+        if(err){
+            callback(err);
+            console.log(err)
+        }else{ 
+            callback(data)
+        }
+    })
+    connection.end()
+}
+
+function delAnnounce(sqlWord,callback){
+    let connection = mysql(); 
+    let query = "delete from public_announce where id = ?"; 
+    connection.query(query,sqlWord,(err,data)=>{
+        if(err){
+            callback(err);
+            console.log(err)
+        }else{ 
+            callback(data)
+        }
+    })
+    connection.end()
+}
 
 module.exports = {
     getAnnounceInfo, 
+    deliverAnnounce,
+    getMyDelivetAnnounce,
+    delAnnounce
 }
 
--- router/announceRouter.js
+++ router/announceRouter.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-03-28 17:42:08
- * @LastEditTime: 2020-03-28 17:47:34
+ * @LastEditTime: 2020-03-29 13:51:07
  * @LastEditors: Please set LastEditors
  * @Description: 学院公告
  * @FilePath: \毕业设计\server\router\announceRouter.js
@@ -13,25 +13,26 @@ let url = require('url')
 
 module.exports = (app) => {
 
-    // app.post('/addJobInfo',(req,res)=>{
-    //     let params = req.body; 
-    //     sqlFunc.addJobInfo(params,(data)=>{
-    //         if(data.affectedRows){
-    //             res.send(JSON.stringify({
-    //                 statusCode: 200,
-    //                 message: '保存成功'
-    //             }));
-    //         }else{
-    //             res.send(JSON.stringify({
-    //                 statusCode: 500,
-    //                 message: '服务器错误'
-    //             }));
-    //         }
-    //     })
-    // })
+    app.post('/deliverAnnounce',(req,res)=>{
+        let params = req.body; 
+        console.log(params)
+        sqlFunc.deliverAnnounce(params,(data)=>{
+            if(data.affectedRows){
+                res.send(JSON.stringify({
+                    statusCode: 200,
+                    message: '保存成功'
+                }));
+            }else{
+                res.send(JSON.stringify({
+                    statusCode: 500,
+                    message: '服务器错误'
+                }));
+            }
+        })
+    })
 
     app.post('/getAnnounceInfo',(req,res)=>{ 
-        sqlFunc.getAnnounceInfo('',(data)=>{
+        sqlFunc.getAnnounceInfo(req.body,(data)=>{
             if(data.length){
                 res.send(JSON.stringify({
                     data
@@ -44,6 +45,36 @@ module.exports = (app) => {
             }
         })
     })
+
+    app.post('/getMyDelivetAnnounce',(req,res)=>{ 
+        sqlFunc.getMyDelivetAnnounce(req.body.userid,(data)=>{
+            if(data.length){
+                res.send(JSON.stringify({
+                    statusCode:200,
+                    data
+                }));
+            }else{
+                res.send(JSON.stringify({
+                    statusCode: 500,
+                    message: '服务器错误'
+                }));
+            }
+        })
+    })
     
+    app.post('/delAnnounce',(req,res)=>{ 
+        sqlFunc.delAnnounce(req.body.id,(data)=>{
+            if(data){
+                res.send(JSON.stringify({
+                    statusCode:200,
+                }));
+            }else{
+                res.send(JSON.stringify({
+                    statusCode: 500,
+                    message: '服务器错误'
+                }));
+            }
+        })
+    })
    
 }
